20ea7dd03dafce412488992753a37b486e9b70a7
CryptoChris <c.peters@makermaker.de>
2022-02-10 07:53:59 +0100

    user register and added user as creator for a campaign.

--- src/index.js
+++ src/index.js
@@ -1,65 +1,106 @@
-const express = require("express")
-const cors = require("cors")
-const jwt = require("jsonwebtoken")
-const dotenv = require("dotenv")
-dotenv.config()
-const app = express()
-const PORT = 9898
-const { createCampaign, checkUser } = require("./utils/db.js")
-let counts = 0
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+const PORT = 9898;
+const { createCampaign, checkUser, getCampaigns, createUser } = require("./utils/db.js");
+let counts = 0;
 
 let tokenCreation = (req, res, next) => {
-    checkUser({
-        name: req.body.user,
-        password: req.body.pass,
-    }).then((data) => console.log("HIER IST DER USER", data[0]._id))
-    // token needs exp time etc.
-    let token = jwt.sign({ user: req.body.user }, process.env.TOKEN_SECRET)
-    res.locals.token = token
-    next()
-}
+	checkUser({
+		name: req.body.user,
+		password: req.body.pass,
+	}).then((data) => {
+		if (data.length > 0) {
+			let token = jwt.sign(
+				{ user: data[0]._id },
+				process.env.TOKEN_SECRET,
+				{ expiresIn: "15m" }
+			);
+			res.locals.token = token;
+			next();
+		} else {
+			res.status(401).json({ error: "User not found" });
+		}
+	});
+
+	// token needs exp time etc.
+};
 
 let tokenCheck = (req, res, next) => {
-    // token aus dem request fischen
-    let token = req.headers.authorization
-    token = token.split(" ")
-    token = token[1]
-    console.log(token)
-    try {
-        const check = jwt.verify(token, process.env.TOKEN_SECRET)
-        console.log("SUPER DUPER")
-    } catch (err) {
-        console.log("HURRA EIN FEHLER IST DA")
-    }
-    // token veryfien
-    next()
-}
+	let token = req.headers.authorization;
+	token = token.split(" ");
+	token = token[1];
+	try {
+		const check = jwt.verify(token, process.env.TOKEN_SECRET);
+		res.locals.verify = true;
+		res.locals.decoded = check;
+	} catch (err) {
+		res.locals.verify = false;
+		res.locals.errorMessage = err.name;
+		console.log("HURRA EIN FEHLER IST DA", err.name);
+	}
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+	next();
+};
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/api/", (req, res) => {
-    res.end("Welcome")
-})
+	res.end("Welcome");
+});
 
 app.post("/api/login", tokenCreation, (req, res) => {
-    console.log(res.locals.token)
-    res.status(200).json({ token: res.locals.token })
-})
+	res.status(200).json({ token: res.locals.token });
+});
+app.post("/api/register", (req,res) => {
+	let user = { name: req.body.user,
+		password: req.body.pass,}
+	console.log('Unser USer: ', user)
+	createUser(user)
+	res.status(200).end()
+});
 app.post("/api/createcampaign", tokenCheck, async (req, res) => {
-    createCampaign(req.body)
-    res.status(200).end()
-})
+	if (res.locals.verify) {
+		let campaign = req.body;
+		campaign.creator = res.locals.decoded.user;
+		createCampaign(campaign);
+		res.status(200).end();
+	} else {
+		res.status(401).json({
+			verify: res.locals.verify,
+			message: res.locals.errorMessage,
+		});
+	}
+});
+
+app.get("/api/campaigns", tokenCheck, async (req, res) => {
+	if (res.locals.verify !== true) {
+		res.status(401).json({
+			verify: res.locals.verify,
+			message: res.locals.errorMessage,
+		});
+	}
+	getCampaigns(res.locals.decoded.user).then((campaings) =>
+		res.status(200).json(campaings)
+	);
+});
 
 app.get("/api/validate", tokenCheck, (req, res) => {
-    res.status(200).end()
-})
+	console.log("Nach verify: ", res.locals.verify);
+	res.status(res.locals.verify ? 200 : 401).json({
+		verify: res.locals.verify,
+	});
+});
 app.get("/counter", (req, res) => {
-    counts++
+	counts++;
 
-    res.status(200).json({ count: counts })
-})
+	res.status(200).json({ count: counts });
+});
 app.listen(PORT, () => {
-    console.log("Server listen on port:", PORT)
-})
+	console.log("Server listen on port:", PORT);
+});
--- src/utils/db.js
+++ src/utils/db.js
@@ -1,24 +1,45 @@
-const { MongoClient } = require("mongodb")
-const uri = process.env.DB_URL
-const client = new MongoClient(uri)
+const { MongoClient } = require("mongodb");
+const uri = process.env.DB_URL;
+const client = new MongoClient(uri);
 
 async function createCampaign(campaign) {
-	await client.connect()
-	const result = await client.db('tablewars').collection('campaigns').insertOne(campaign)
+	await client.connect();
+	const result = await client
+		.db("tablewars")
+		.collection("campaigns")
+		.insertOne(campaign);
 }
 
 async function createUser(user) {
-	await client.connect()
-	const result = await client.db('tablewars').collection('users').insertOne(user)
+	await client.connect();
+	const result = await client
+		.db("tablewars")
+		.collection("user")
+		.insertOne(user);
 }
 
 async function checkUser(user) {
-	await client.connect()
-	const result = await client.db('tablewars').collection('user').find(user).toArray()
-	return result
+	await client.connect();
+	const result = await client
+		.db("tablewars")
+		.collection("user")
+		.find(user)
+		.toArray();
+	return result;
 }
 
+async function getCampaigns(user) {
+	await client.connect();
+	const result = await client
+		.db("tablewars")
+		.collection("campaigns")
+		.find({ creator: user })
+		.toArray();
+	return result;
+}
 module.exports = {
 	createCampaign,
 	checkUser,
-}
+	getCampaigns,
+	createUser,
+};
